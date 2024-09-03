b298fcc331f97dd6ae25168f4171667df87c122a
ychan0303 <ychan0397@gmail.com>
2024-09-03 22:43:29 +0900

    [solution] q_002.
    - Solution2 수정
    - 답안 참고

--- Q_002/chan/Solution2.java
+++ Q_002/chan/Solution2.java
@@ -8,45 +8,47 @@ public class Solution2 {
 
     public static void main(String[] args) throws IOException {
 
-        // 수열의 길이
+        // 입력받을 수열의 길이
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sequenceLength = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         // 스택
         Stack<Integer> stack = new Stack<>();
 
-        // 결과
+        // 결과 (+, -)
         ArrayList<String> resultList = new ArrayList<>();
 
-        // 사용자 입력 값
-        int inputListIndex = 0;
-        ArrayList<Integer> inputList = new ArrayList<>();
-        for (int i = 0; i < sequenceLength; i++) inputList.add(Integer.parseInt(br.readLine()));
+        // 마지막으로 push 된
+        int maxPushed = 0;
 
-        // 수열을 만들 수 있는지
-        for (int i = 1; i <= sequenceLength; i++) {
-            // 1부터 차례로 push
-            stack.push(i);
-            resultList.add("+");
+        // 입력된 수열을 처리
+        while (n-- > 0) {
 
-            // 입력값과 동일한 경우 pop
-            while (!stack.empty() && stack.peek() == inputList.get(inputListIndex)) {
-                stack.pop();
-                resultList.add("-");
-                inputListIndex++;
+            int currentNumber = Integer.parseInt(br.readLine());
 
+            // 현재 스택에 푸시된 가장 큰 수보다 더 큰 수가 입력된 경우
+            if (maxPushed < currentNumber) {
+                for (int i = maxPushed + 1; i <= currentNumber; i++) {
+                    stack.push(i);
+                    resultList.add("+");
+                }
+                // 현재 입력된 수를 가장 큰 푸시된 수로 갱신
+                maxPushed = currentNumber;
+            }
+            // 스택의 맨 위 값이 현재 입력된 수와 다르면, 수열을 만들 수 없음
+            else if (stack.peek() != currentNumber) {
+                System.out.println("NO");
+                return;
             }
 
+            // 스택의 맨 위 값을 팝하고, "-" 연산 기록
+            stack.pop();
+            resultList.add("-");
         }
 
-        if (stack.empty()) {
-            // 스택을 다 비웠으면 성공
-            for (String result : resultList) System.out.println(result);
-
-        } else {
-            // 스택을 다 비우지 못했으면 실패
-            System.out.println("NO");
-
+        // 결과 출력
+        for (String result : resultList) {
+            System.out.println(result);
         }
     }
 
